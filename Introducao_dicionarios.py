0feb5a83bf0006182acc942f5bf82bf15c4d8bcf
Edson-Padilha <60894182+Edson-Padilha@users.noreply.github.com>
2021-01-10 22:57:48 -0300

    Add files via upload

---
+++ Introducao_dicionarios.py
@@ -0,0 +1,14 @@
+print()
+d1 = {}
+print(type(d1))
+print()
+
+d1 ['bbb'] = 2000
+d1 ['ccc'] = 3000
+d1 ['aaa'] = 1000
+print(d1)
+print()
+print('>>>>Exemplo 2')
+d2 = {1.1:"teste1", 2.2:"teste2",3:"teste3"}
+print(d2)
+print()
