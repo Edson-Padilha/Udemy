f3e5e983b54d4350ee0e24782da989226b777fdb
Edson-Padilha <60894182+Edson-Padilha@users.noreply.github.com>
2021-01-13 12:07:12 -0300

    Add files via upload

---
+++ Instrucao_nonlocal.py
@@ -0,0 +1,16 @@
+print()
+
+def func():
+
+    var_local = 10
+
+    def func_interna():
+        nonlocal var_local
+        var_local += 1
+        print(var_local)
+
+    func_interna()
+
+func()
+
+print()
