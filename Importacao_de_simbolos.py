dc954a84baaa55dfada31bb7bd504e8adba02e90
Edson-Padilha <60894182+Edson-Padilha@users.noreply.github.com>
2021-01-17 22:13:29 -0300

    Add files via upload

---
+++ Importacao_de_simbolos.py
@@ -0,0 +1,12 @@
+from math import pi, e
+
+def func():
+    from math import factorial # Pode-se importar em qualquer local, não é regra no inicio.
+    print(factorial(10))
+
+func()
+
+print(pi)
+print(e)
+
+# Para importar todos os simbolos de um modulo basta colocar *
